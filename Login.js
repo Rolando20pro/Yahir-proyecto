f753b606b1195811f0c40fc6a23961ced73d42b1
Rolando20pro <rolando.barbosa.alp@cbtis258.edu.mx>
2024-10-24 14:06:21 -0600

    Create Login.js

---
+++ Login.js
@@ -0,0 +1,29 @@
+function handleLogin(event) {
+
+    event.preventDefault(); 
+
+
+
+   
+
+    const username = document.getElementById('username').value;
+
+    const password = document.getElementById('password').value;
+
+
+
+    
+
+    if (username === 'admin' && password === 'password') { 
+
+        
+
+        window.location.href = 'pagina1.html'; 
+
+    } else {
+
+        alert('Usuario o contraseña incorrectos.');
+
+    }
+
+}
